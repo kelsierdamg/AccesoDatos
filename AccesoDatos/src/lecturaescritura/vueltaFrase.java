d1b5165f35d8cdc78ad47f846182243a58ffea98
alumnoTarde <alumnoTarde@DESKTOP-F70SV68>
2025-09-25 20:54:01 +0200

    commit 3

--- AccesoDatos/src/lecturaescritura/vueltaFrase.java
+++ AccesoDatos/src/lecturaescritura/vueltaFrase.java
@@ -2,6 +2,7 @@ package lecturaescritura;
 
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class vueltaFrase {
 	public static void main(String[] args) {
@@ -10,18 +11,16 @@ public class vueltaFrase {
 		}
 		
 		try(FileReader fr = new FileReader(args[0])) {
+			ArrayList<Character> caracteres = new ArrayList<>();
 			int n;
 			while((n=fr.read()) != -1) {
 				System.out.print((char)n);
+				caracteres.add((char)n);
 			}
 			
-			char arr2[] = new char[n];
-			int longitud = arr2.length;
-			char arr1[] = new char[longitud];
-			
-			for(int i=20; i>0; i--) {
-				arr1[n]=arr2[i];
-				n++;
+			System.out.println();
+			for(int i=caracteres.size()-1; i>=0; i--) {
+				System.out.print(caracteres.get(i));
 			}
 		}
 		catch(IOException e) {
